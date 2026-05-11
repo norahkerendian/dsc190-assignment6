01ee8c35a141c4f2602fc7c36e83f01262d8ed9b
norahkerendian <norahkerendian@gmail.com>
2026-05-11 10:01:37 -0700

    wip: implement parse function for natural language dates

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,7 +6,7 @@ def test_tomorrow():
     assert parser.parse("tomorrow", today=date(2025,1,1)) == date(2025,1,2)
 
 def test_a_week():
-    assert parser.parse("a week from today", today=date(2025,1,1)) == date(2025,1,7)
+    assert parser.parse("a week from today", today=date(2025,1,1)) == date(2025,1,8)
 
 def test_yesterday():
     assert parser.parse("yesterday", today=date(2025,1,2)) == date(2025,1,1)
@@ -27,7 +27,7 @@ def test_next_tuesday():
     assert parser.parse("Next Tuesday", today=date(2025,1,1)) == date(2025, 1,7)
 
 def test_two_weeks_from_tomorrow():
-    assert parser.parse("two weeks from tomorrow", today=date(2025,1,1)) == date(2025, 1, 15)
+    assert parser.parse("two weeks from tomorrow", today=date(2025,1,1)) == date(2025, 1, 16)
 
 def test_one_year_and_2_months_after_yesterday():
-    assert parser.parse("1 year and 2 months after yesterday", today=date(2025,1,2)) == date(2027,3,1)
+    assert parser.parse("1 year and 2 months after yesterday", today=date(2025,1,2)) == date(2026,3,1)
